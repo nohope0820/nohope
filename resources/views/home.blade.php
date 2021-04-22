920e1ae1eae4e7c1bb9e9f5e3e13a0b950a82a11
NO HOPE <nohope.nht@gmail.com>
2021-04-22 13:14:33 +0700

    blog_chat

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+   <div class="content">
+          <!-- nội dung trái - menu -->
+          <div class="menu" style="margin-left: 200px;">
+             <ul>
+           <li><a href=""> BẠN BÈ </a></li>
+           <li><a href=""> TÌM BẠN BÈ </a></li>
+           <li><a href=""> PHÒNG </a></li>
+           <li><a href=""> TÌM PHÒNG </a></li>
+           <li><a href=""> TRANG CÁ NHÂN </a></li>
+             </ul>
+           </div>
+           <!-- nội dung chính -->
+           <div class="main">
+                <span>Danh sách bạn bè</span>
+                <ul class="list-friend">
+                     <li></li>
+
+                </ul>
+            </div>
+</div>
+
+
+@endsection
